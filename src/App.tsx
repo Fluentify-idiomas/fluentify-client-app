d5d6d72506b4c66e8cea8538df7e5ad5a9fa2373
igorm_dev <igoribeirom@gmail.com>
2024-04-29 00:07:28 -0300

    fix: Remove unecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import LandingPage from './pages/LandingPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <LandingPage />
