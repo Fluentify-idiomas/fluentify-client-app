926393551166eb58b52de13ac25690582f968659
igorm_dev <igoribeirom@gmail.com>
2024-05-19 23:55:49 -0300

    aa

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://api.fluentify.net.br",
+  baseURL: "https://api.fluentify.net.br",
   // baseURL: 'http://127.0.1:5000',
   timeout: 5000,
 });
